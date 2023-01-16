99f67c56efebb0bd89fdfc569de732e67ff84913
Minhyeok Son <mh.blosson@gmail.com>
2023-01-16 16:48:38 +0900

    [FE][feat] input 컴포넌트 중간완성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
-import Btn from './components/Button'
+import Button from './components/Button'
+import Input from './components/Input'
 
 function App() {
   return (
     <div className="App">
-      <Btn></Btn>
+      <Button></Button>
+      <Input label="이게 레이블임" placeHolder="이게 플레이스호올더임"></Input>
     </div>
   )
 }
