d39c9d13e982ca2af62d1fc0d679123a5e52a02f
Byongho <unlike96@gmail.com>
2023-01-19 13:26:11 +0900

    [FE][feat] Button.js react-icon 라이브러리 적용

---
+++ frontend/src/components/atoms/Message.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+/*
+  text와 isValid 값을 받아서
+  isValid가 true일 경우 text를 파란색으로, false일 경우 text를 빨간색으로 렌더링 
+*/
+const Message = ({ text, isValid }) => {
+  return <MessageDiv isValid={isValid}>{text}</MessageDiv>
+}
+
+const MessageDiv = styled.div`
+  color: ${(props) =>
+    props.isValid ? props.theme.blueColor : props.theme.redColor};
+  margin-bottom: 1rem;
+`
+export default Message
