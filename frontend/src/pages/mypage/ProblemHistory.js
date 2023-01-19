f5e9c913a668b7a0541adc3e7f19ac7c480ea088
Minhyeok Son <mh.blosson@gmail.com>
2023-01-19 13:18:12 +0900

    [FE][feat] Side Navbar 완성

--- frontend/src/pages/mypage/ProblemHistory.js
+++ frontend/src/pages/mypage/ProblemHistory.js
@@ -1,8 +1,9 @@
+import ProblemList from './ProblemList'
+
 const ProblemHistory = () => {
   return (
     <div>
-      <h1>내가 푼 문제 목록</h1>
-      <p>내가 푼 문제 목록 페이지입니다.</p>
+      <ProblemList />
     </div>
   )
 }
