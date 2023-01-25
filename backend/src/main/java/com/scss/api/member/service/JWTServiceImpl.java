23557b1379bc3dd0ce8c4e732b93eaa7a62bd93e
이찬희 <dojsfffff@naver.com>
2023-01-25 17:45:19 +0900

    [BE][feat] JWT 검증 예외처리 및 설정파일 변경 secret key 값을 안전하게 관리하기 위해서 따로 빼내기

--- backend/src/main/java/com/scss/api/member/service/JWTServiceImpl.java
+++ backend/src/main/java/com/scss/api/member/service/JWTServiceImpl.java
@@ -6,40 +6,50 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.security.Key;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class JWTServiceImpl implements JWTService{
 
-    private static final Logger logger = LoggerFactory.getLogger(MemberServiceImpl.class);
-    // 생성자 주입
-    private final MemberMapper memberRepository;
+    private static final Logger logger = LoggerFactory.getLogger(JWTServiceImpl.class);
 
-    private static final String SECRET_KEY = "Vecx3vjPvfdOdvvnQWfd30vrfo0zEVSD39vZed0k4jfdosvdf33dVRFidczjjfdijvodivjsognsjvbdabEFSDvidjvVDseoivjosfj";
+    //private static final String SECRET_KEY = "Vecx3vjPvfdOdvvnQWfd30vrfo0zEVSD39vZed0k4jfdosvdf33dVRFidczjjfdijvodivjsognsjvbdabEFSDvidjvVDseoivjosfj";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
     private static final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
-    // 토큰 생성
+    /** 토큰 생성 **/
     @Override
     public String createToken(String id, String subject, long expTime) {
+        logger.debug("토큰 생성 시작");
+        logger.debug("secret key : {}", SECRET_KEY);
         byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
         Key signinKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
+        Date now = new Date();
+
+
+        logger.debug("{} 토큰 만료 시간 !!!!!!!! : {}", subject, new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis() + expTime));
 
         return Jwts.builder()
                 .setSubject(subject) // access or refresh
                 .claim("member_id", id)
                 .signWith(signinKey, signatureAlgorithm)
-                .setExpiration(new Date(System.currentTimeMillis() * expTime)) // 토큰 만료시간 설정
+                .setExpiration(new Date(System.currentTimeMillis() + expTime))
                 .compact();
     }
 
-    // 토큰 검증
+    /** 토큰 검증 **/
     @Override
     public Claims getToken(String token) {
         try {
