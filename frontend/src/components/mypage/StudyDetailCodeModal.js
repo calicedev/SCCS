c5b99155ec35e0176a242292ab0cd75413bb8e0f
Minhyeok Son <mh.blosson@gmail.com>
2023-02-02 09:57:54 +0900

    [FE][fix] 스터디 기록 세부 페이지 컴포넌트 상속 오류 해결

--- frontend/src/components/mypage/StudyDetailCodeModal.js
+++ frontend/src/components/mypage/StudyDetailCodeModal.js
@@ -1,18 +1,18 @@
-// import React from 'react'
-// import styled from 'styled-components'
+import React from 'react'
+import styled from 'styled-components'
 
-// export default function StudyDetailCodeModal({ code }) {
-//   return (
-//     <ModalContainer>
-//       <h4>{code.member_id}의 코드</h4>
-//       <div>{code.code}</div>
-//     </ModalContainer>
-//   )
-// }
+export default function StudyDetailCodeModal({ code }) {
+  return (
+    <ModalContainer>
+      <h4>{code.submissionMemberId}의 코드</h4>
+      <div>{code.sumbmissionStoreFileName}</div>
+    </ModalContainer>
+  )
+}
 
-// const ModalContainer = styled.div`
-//   margin-top: 20px;
-//   padding: 20px;
-//   background-color: #61dafb;
-//   text-align: left;
-// `
+const ModalContainer = styled.div`
+  margin-top: 20px;
+  padding: 20px;
+  background-color: #61dafb;
+  text-align: left;
+`
