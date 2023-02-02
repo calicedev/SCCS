cb22c1931d106ce1d8691acb1bbf911433c676b4
임혜은 <calicedev@gmail.com>
2023-02-02 13:31:45 +0900

    [BE][feat] 오류 안나는 웹소켓 작업중

--- backend/src/main/java/com/scss/config/WebSocketConfig.java
+++ backend/src/main/java/com/scss/config/WebSocketConfig.java
@@ -1,36 +1,28 @@
 package com.scss.config;
-
-import com.scss.handler.CustomHandshakeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@EnableWebSocketMessageBroker //Stomp사용하기 위해서 선언
 @Configuration
-//Stomp사용 = WebSocketMessageBrokerConfigurer 상속
-//WebSocket사용 = WebSocketConfigurer 상속
+@EnableScheduling
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        //메시지를 구독하는 요청의 prefix는 /sub로 시작하도록 설정
-        config.enableSimpleBroker("/sub");
-
-        //서버에 있는 모든 user가 메시지를 받도록
-        //메시지를 발행하는 요청의 prefix는 /pub으로 시작하도록 설정
-        config.setApplicationDestinationPrefixes("/pub");
-    }
-
-    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //stomp websockey의 연결 endpoint는 /stomp-game으로 설정
         registry.addEndpoint("/sccs")
                 .setAllowedOriginPatterns("*")
-                //내가 만든 핸들러 실행할 수 있도록 등록
-                .setHandshakeHandler(new CustomHandshakeHandler())
                 .withSockJS();
+
     }
 
+    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
+    }
 }
