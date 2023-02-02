8fe5b2febb6ff25c22d4a7be705a33d8bb13828d
임혜은 <calicedev@gmail.com>
2023-02-02 11:43:12 +0900

    [BE][feat] 웹소켓 api 수정 중

--- backend/src/main/java/com/scss/api/socket/dto/SocketDto.java
+++ backend/src/main/java/com/scss/api/socket/dto/SocketDto.java
@@ -1,15 +1,17 @@
 package com.scss.api.socket.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-// Data 어노테이션은 getter, setter를 자동 생성합니다.
-@Data
-
-// AllArgsConstructor 어노테이션은 생성자를 자동 생성합니다.
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SocketDto {
-    // 유저의 이름을 저장하기 위한 변수
-    private String userName;
-    private String content;
+    private int StudyroomId;
+    private String nickname;
+    private String message;
+    private boolean status;
 }
--- backend/src/main/java/com/scss/config/WebSocketConfig.java
+++ backend/src/main/java/com/scss/config/WebSocketConfig.java
@@ -1,24 +1,36 @@
 package com.scss.config;
-import org.springframework.beans.factory.annotation.Configurable;
+
+import com.scss.handler.CustomHandshakeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-@Controller
-@Configurable
-@EnableWebSocketMessageBroker
+
+@EnableWebSocketMessageBroker //Stomp사용하기 위해서 선언
+@Configuration
+//Stomp사용 = WebSocketMessageBrokerConfigurer 상속
+//WebSocket사용 = WebSocketConfigurer 상속
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        //메시지를 구독하는 요청의 prefix는 /sub로 시작하도록 설정
+        config.enableSimpleBroker("/sub");
+
+        //서버에 있는 모든 user가 메시지를 받도록
+        //메시지를 발행하는 요청의 prefix는 /pub으로 시작하도록 설정
+        config.setApplicationDestinationPrefixes("/pub");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/test").setAllowedOriginPatterns("*").withSockJS();
+        //stomp websockey의 연결 endpoint는 /stomp-game으로 설정
+        registry.addEndpoint("/sccs")
+                .setAllowedOriginPatterns("*")
+                //내가 만든 핸들러 실행할 수 있도록 등록
+                .setHandshakeHandler(new CustomHandshakeHandler())
+                .withSockJS();
     }
 
 }
