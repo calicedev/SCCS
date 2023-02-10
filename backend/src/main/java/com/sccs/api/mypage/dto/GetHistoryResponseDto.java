c81526d6ebbb0faefcdfafcb3c57f9490b629bdb
Midori <holicmiku.15@gmail.com>
2023-02-10 10:20:46 +0900

    [BE][refactor] MypageController 수정 S3 도입으로 인한 파일 URL 처리

--- backend/src/main/java/com/sccs/api/mypage/dto/GetHistoryResponseDto.java
+++ backend/src/main/java/com/sccs/api/mypage/dto/GetHistoryResponseDto.java
@@ -11,13 +11,6 @@ import lombok.Setter;
 @ApiModel("GetHistoryGetResponse")
 public class GetHistoryResponseDto {
 
-  public class problem {
-
-    @ApiModelProperty(example = "문제 이름")
-    private String problemName;
-    @ApiModelProperty(example = "문제 번호")
-    private int problemId;
-  }
 
   @ApiModelProperty(example = "스터티룸이 만들어진 시간")
   private String studyroomCreateDatetime;
@@ -25,7 +18,7 @@ public class GetHistoryResponseDto {
   private String studyroomTitle;
   @ApiModelProperty(example = "스터티룸의 고유아이디")
   private int studyroomId;
-  @ApiModelProperty(example = "스터티룸에서 풀은 문제들")
-  private List<problem> problems;
+  @ApiModelProperty(example = "스터티룸에서 풀은 문제들", dataType = "list")
+  private List<ProblemResponseDto> problems;
 
 }
