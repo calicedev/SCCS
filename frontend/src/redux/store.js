b1646af9e7aa2dbfdda72d88df6d7ccd3cf05569
손민혁 <mh.blosson@gmail.com>
2023-02-12 00:48:44 +0900

    [FE][fix] 첫 코테 문제 지정 오류, 스터디 세부 페이지 오류 해결

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -16,7 +16,7 @@ const reducers = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'],
+  // blacklist: ['user'],
 }
 
 const persistedReducer = persistReducer(persistConfig, reducers)
