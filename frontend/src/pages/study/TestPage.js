5956a48f2ecb2386f3b8da119d54ba130943222b
Minhyeok Son <mh.blosson@gmail.com>
2023-02-14 10:49:16 +0900

    [FE][feat] 테스트 페이지 언어 변경 기능

--- frontend/src/pages/study/TestPage.js
+++ frontend/src/pages/study/TestPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import styled from 'styled-components'
+import { useOutletContext } from 'react-router-dom'
 import { Resizable } from 're-resizable'
 
 // import Footer from 'components/study/Footer'
@@ -10,192 +11,200 @@ import api from 'constants/api'
 import Button from 'components/common/Button'
 import Textarea from 'components/study/Textarea'
 import { algorithmPk, languagePk } from 'constants/pk'
+import useInterval from 'hooks/useInterval'
+import OutlineButton from 'components/common/OutlineButton'
+import ProblemImage from 'components/common/ProblemImage'
+import CodeSection from 'components/study/CodeSection'
 
-export default function CodingTest({
-  studyroomId,
-  membersNickname,
-  roomInfo,
-  personnel,
-  startStudy,
-  id,
-  nickname,
-  dataForStudy,
-  setDataForStudy,
-}) {
-  const [codingTestData, setCodingTestData] = useState({})
+import RoomInfo from 'components/study/RoomInfo'
+import ResultSection from 'components/study/ResultSection'
+import Loading from 'components/common/Loading'
+
+const initialCode = {
+  1: `class Solution:
+  print("sccs")
+`,
+  2: `class Solution{
+  public static void main(String[] args) {
+    System.out.println("sccs");
+  }
+}
+`,
+}
+
+export default function TestPage() {
+  const {
+    user,
+    studyroomId,
+    roomInfo,
+    stomp,
+    connected,
+    members,
+    setMembers,
+    problems,
+    setProblems,
+    message,
+    setMessage,
+    chatList,
+    sendChat,
+    disconnect,
+  } = useOutletContext()
+
+  const navigate = useNavigate()
+
+  const [subscription, setSubscription] = useState(null)
+
+  const [problemIdx, setProblemIdx] = useState(0) // 현재 선택된 문제의 인덱스
+  const [finished, setFinished] = useState(false)
+  const [finishedList, setFinishedList] = useState([])
+  const [languageId, setLanguageId] = useState(roomInfo.languageIds[0])
+
+  const [code, setCode] = useState(initialCode[languageId])
 
   // 남은 시간 표시하기 위한 state
-  const [startTime, setStartTime] = useState(Date.now())
-  const [currentTime, setCurrentTime] = useState(Date.now())
-  const [timeLeft, setTimeLeft] = useState(7200000)
+  const [timer, setTimer] = useState(2 * 60 * 60)
 
-  // 문제 선택하기 위한 state
-  const [onProblem, setOnProblem] = useState() // 현재 선택된 문제의 pk
-  const [onProblemIdx, setOnProblemIdx] = useState(0) // 현재 선택된 버튼의 idx (이미지 표시하기 위함)
-  const [problemArray, setProblemArray] = useState([]) // 문제 pk 리스트 ex) [7, 19]
-
-  // 정보 저장해보자
+  const [testResult, setTestResult] = useState(null)
 
   // 코딩테스트 페이지 입장 시 axios 요청
   useEffect(() => {
     const data = {
       id: studyroomId,
-      memberIds: membersNickname,
+      nickname: user.nickname,
+      memberIds: members,
     }
     const [url, method] = api('codingTest')
     const config = { url, method, data }
-    console.log('방장이 보낸 정보 받아라', data)
     axios(config)
       .then((res) => {
-        setCodingTestData(res.data)
-        console.log(res.data.problems[0].id, res.data.problems[1].id)
-        // 첫번째 문제현재 선택된 문제로 지정해서 제출 시에 넘어갈 수 있게 해줌
-        setOnProblem(res.data.problems[0].id)
-        setProblemArray([
-          ...problemArray,
-          res.data.problems[0].id,
-          res.data.problems[1].id,
-        ])
-        setDataForStudy([
-          ...dataForStudy,
-          res.data.problems[0].id,
-          res.data.problems[1].id,
-        ])
+        setProblems(res.data.problems)
       })
-      .catch((err) => {})
+      .catch(() => {})
   }, [])
 
-  // 남은 시간 카운트 다운 useEffect
+  // 문제 번호나, 언어 변경 시 초기화
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentTime(Date.now())
-      setTimeLeft(7200000 + startTime - currentTime)
-    }, 1000)
+    setCode(initialCode[languageId])
+    setTestResult(null)
+  }, [problemIdx, languageId])
 
-    return () => clearInterval(intervalId)
-  }, [startTime, currentTime])
+  // 모두 시험을 종료하면 테스트 페이지로 이동
+  useEffect(() => {
+    if (finishedList.length === members.length) {
+      navigate(`/room/${studyroomId}/study`)
+    }
+  }, [finishedList])
 
-  const hours = Math.floor(
-    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-  )
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
+  // 웹 소켓 send: 시험 종료
+  const sendFinished = () => {
+    stomp.send(
+      '/pub/studyroom',
+      {},
+      JSON.stringify({
+        status: 'study',
+        studyroomId: studyroomId,
+        nickname: user.nickname,
+      }),
+    )
+    setFinished(true)
+  }
 
-  //---------------------------------------------------------------------------------------------------
-  const [codingTestResult, setCodingTestResult] = useState([])
-  const navigate = useNavigate()
-
-  const testCode = useCallback(() => {
-    // let fileName = 'formFile.txt';
-    const content = document.querySelector('textarea').value
-    const element = document.createElement('a')
-    const file = new Blob([content], { type: 'text/plain' })
-    const formData = new FormData()
-    formData.append('formFile', file)
-    formData.append('memberId', id)
-    formData.append('studyroomId', studyroomId)
-    console.log(onProblem)
-    // 지금은 채점 서버에 1번 문제밖에 없어서 이렇게 하지만 더 들어오면 OnProblem으로 해야함 (2.10 민혁)
-    // formData.append('problemId', 1)
-    formData.append('problemId', onProblem)
-    // 지금은 Java(2)로 고정하지만 나중에는 파이썬 or 파이썬+자바의 경우도 넣어줘야함 (2.10 민혁)
-    formData.append('languageId', 2)
-
-    const headers = { 'Content-Type': 'multipart/form-data' }
-    const [url, method] = api('testCode')
-    const config = { url, method, data: formData, headers }
-    axios(config)
-      .then((res) => {
-        setCodingTestResult(res.data)
-        // navigate('/')
-      })
-      .catch((err) => {})
-    // element.href = URL.createObjectURL(file);
-    // // element.download = fileName;
-    // document.body.appendChild(element); // Required for this to work in FireFox
-    element.click()
+  // 웹 소켓 subscribe
+  useEffect(() => {
+    setSubscription(
+      stomp.subscribe('/sub/studyroom/' + studyroomId, (chatDto) => {
+        const content = JSON.parse(chatDto.body)
+        if (content.status === 'study') {
+          setFinishedList((finishedList) => [...finishedList, chatDto.nickname])
+        }
+      }),
+    )
+    return () => {
+      subscription && subscription.unsubscribe()
+    }
   }, [])
 
-  const submitCode = () => {
-    // let fileName = 'formFile.txt';
-    const content = document.querySelector('textarea').value
-    const file = new Blob([content], { type: 'text/plain' })
+  // 타이머
+  useInterval(() => setTimer((timer) => timer - 1), 1000)
+
+  useEffect(() => {
+    if (timer <= 0) {
+      navigate(`/room/${studyroomId}/study`)
+    }
+  }, [timer])
+
+  // 코드 제출
+  const submitCode = (type) => {
+    const apiKey = type === 'submit' ? 'submitCode' : 'testCode'
+    const codeString = code
     const formData = new FormData()
-    formData.append('formFile', file)
-    formData.append('memberId', id)
+    formData.append('formFile', new Blob([codeString], { type: 'text/plain' }))
+    formData.append('memberId', user.id)
     formData.append('studyroomId', studyroomId)
-    console.log(onProblem)
-    // 지금은 채점 서버에 1번 문제밖에 없어서 이렇게 하지만 더 들어오면 OnProblem으로 해야함 (2.10 민혁)
-    // formData.append('problemId', 1)
-    formData.append('problemId', onProblem)
-    // 지금은 Java(2)로 고정하지만 나중에는 파이썬 or 파이썬+자바의 경우도 넣어줘야함 (2.10 민혁)
-    formData.append('languageId', 2)
-
+    formData.append('problemId', problems[problemIdx].id)
+    formData.append('languageId', languageId)
+    // formData.append('problemId', 16)
+    // formData.append('languageId', 2)
     const headers = { 'Content-Type': 'multipart/form-data' }
-
-    // console.log(formData)
-    const [url, method] = api('submitCode')
+    const [url, method] = api(apiKey)
     const config = { url, method, data: formData, headers }
     axios(config)
       .then((res) => {
-        // navigate('/')
-        setCodingTestResult(res.data)
+        setTestResult(res.data)
       })
-      .catch((err) => {})
-    // element.href = URL.createObjectURL(file);
-    // // element.download = fileName;
-    // document.body.appendChild(element); // Required for this to work in FireFox
-    // element.click()
+      .catch(() => {})
   }
-  //---------------------------------------------------------------------------------------------------
 
-  const changeProblem = (idx) => {
-    // console.log(problemArray)
-    setOnProblem(problemArray[idx])
-    setOnProblemIdx(idx)
-    // console.log(onProblem)
-  }
+  const [windowHeight, setWindowHeight] = useState(0)
+
+  useEffect(() => {
+    const updateMaxHeight = () => {
+      setWindowHeight(window.innerHeight)
+    }
+
+    window.addEventListener('resize', updateMaxHeight)
+    updateMaxHeight()
+
+    return () => {
+      window.removeEventListener('resize', updateMaxHeight)
+    }
+  }, [])
+
   return (
     <>
-      {codingTestData.title && (
-        <>
-          <TopNavBar>
-            <Btn>
-              {studyroomId}번방 {roomInfo.title}
-            </Btn>
-            {roomInfo.languageIds.map((languageId, idx) => {
-              return <Btn key={idx}>{languagePk[languageId]}</Btn>
-            })}
-
-            {roomInfo.algoIds.map((algoId, idx) => {
-              return <Btn key={idx}>#{algorithmPk[algoId]}</Btn>
-            })}
-
-            {['1번', '2번'].map((problem, idx) => {
-              return (
-                <Btn
-                  onClick={() => {
-                    changeProblem(idx)
-                  }}
-                  key={idx}
-                >
-                  {problem}
-                </Btn>
-              )
-            })}
-            <span>
-              남은 시간: {hours}시간 {minutes}분 {seconds}초
-            </span>
-            <span> 현재 {personnel}명</span>
-          </TopNavBar>
-
-          <Main>
-            {/* <h3>받은 첫 번째 문제 title : {codingTestData.problems[0].name}</h3> */}
-            <Problem>
-              <Img
-                src={codingTestData.problems[onProblemIdx].problemImageUrl}
-              ></Img>
-            </Problem>
+      {problems ? (
+        <Container>
+          <FlexBox>
+            <ButtonWrapper>
+              <RoomInfo
+                id={roomInfo.id}
+                title={roomInfo.title}
+                languageIds={roomInfo.languageIds}
+                algoIds={roomInfo.algoIds}
+                hostNickname={roomInfo.hostNickname}
+                personnel={roomInfo.personnel}
+              />
+              {[...Array(problems.length).keys()].map((idx) => {
+                return (
+                  <Button
+                    key={`${idx}-problem`}
+                    size="small"
+                    value={idx + 1}
+                    type={problemIdx === idx ? 'primary' : 'secondary'}
+                    onClick={() => setProblemIdx(idx)}
+                  />
+                )
+              })}
+            </ButtonWrapper>
+            <StyledDiv>
+              {parseInt(timer / (60 * 60))}:{parseInt((timer / 60) % 60)}:
+              {parseInt(timer % 60)}
+            </StyledDiv>
+          </FlexBox>
+          <FlexBox2>
+            <ImageWrapper windowHeight={windowHeight}>
+              <ProblemImage imgUrl={problems[problemIdx].problemImageUrl} />
+            </ImageWrapper>
             <Resizable
               defaultSize={{ width: '50%', height: '100%' }}
               minWidth={'20%'}
@@ -211,13 +220,16 @@ export default function CodingTest({
                 topLeft: false,
               }}
             >
-              <FlexColumn>
-                <CodingSection>
-                  <Changer>언어 선택</Changer>
-                  <Textarea value={languagePk}></Textarea>
-                </CodingSection>
+              <FlexColumn windowHeight={windowHeight}>
+                <CodeSection
+                  value={code}
+                  setValue={setCode}
+                  languageIds={roomInfo.languageIds}
+                  languageId={languageId}
+                  setLanguageId={setLanguageId}
+                />
                 <Resizable
-                  defaultSize={{ width: '100%', height: '37%' }}
+                  defaultSize={{ width: '100%', height: '40%' }}
                   minHeight={'20%'}
                   maxHeight={'80%'}
                   enable={{
@@ -231,181 +243,71 @@ export default function CodingTest({
                     topLeft: false,
                   }}
                 >
-                  <ResultSection>
-                    {codingTestResult.map((result, idx) => {
-                      if (idx !== 5) {
-                        return <div key={idx}>{result.result}</div>
-                      } else {
-                        return (
-                          <div key={idx}>
-                            <div>메모리: {result.avgMemory}</div>
-                            <div>실행시간: {result.avgRuntime}ms</div>
-                            <div>
-                              결과:
-                              {result.isAnswer ? (
-                                <span> Success</span>
-                              ) : (
-                                <span> Fail</span>
-                              )}
-                            </div>
-                          </div>
-                        )
-                      }
-                    })}
-                  </ResultSection>
+                  <ResultSection
+                    results={testResult}
+                    isFinished={finished}
+                    finish={sendFinished}
+                    test={() => {
+                      submitCode('test')
+                    }}
+                    submit={() => {
+                      submitCode('submit')
+                    }}
+                  />
                 </Resizable>
-                <ColoredLine color="#4B91F1" />
-
-                <Foot>
-                  <EndBtn onClick={startStudy}>
-                    <Button
-                      value="시험 종료"
-                      type="danger"
-                      size="small"
-                    ></Button>
-                  </EndBtn>
-                  <CompileBtn>
-                    <Button
-                      value="테스트"
-                      type="gray"
-                      size="small"
-                      margin-right="5px"
-                      onClick={() => {
-                        testCode('')
-                      }}
-                    ></Button>
-                    <Space></Space>
-                    <Button
-                      value="제출"
-                      size="small"
-                      onClick={() => {
-                        submitCode('')
-                      }}
-                    ></Button>
-                  </CompileBtn>
-                </Foot>
               </FlexColumn>
             </Resizable>
-          </Main>
-        </>
+          </FlexBox2>
+        </Container>
+      ) : (
+        <Loading height="70vh" />
       )}
     </>
   )
 }
 
-const Main = styled.div`
-  color: #ffffff; // 폰트 색 바꾸려면 이거 바꿔야함
-  display: flex;
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-  background-color: #263747;
-`
-// const Head = styled.div`
-
-// `
-const Adjustment = styled.div`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 5%;
-`
+  gap: 10px;
+  height: 100%;
 
-const Problem = styled.div`
-  height: 100vh;
-  width: calc(40% - 12px);
-  overflow-y: auto;
-  flex: 1;
+  padding: 1rem;
 `
-
-const Img = styled.img`
-  width: 100%;
+const FlexBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const FlexBox2 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 5px;
 `
 
 const FlexColumn = styled.div`
   display: flex;
-  height: 99vh;
   flex-direction: column;
-  flex: 1;
-  overflow: auto visible;
+  gap: 5px;
+  height: ${({ windowHeight }) => `calc(${windowHeight}px - 130px)`};
+`
+const ImageWrapper = styled.div`
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  height: ${({ windowHeight }) => `calc(${windowHeight}px - 130px)`};
+  background-color: ${({ theme }) => theme.whiteColor};
 `
 
-const CodingSection = styled.div`
-  position: relative;
-  padding-top: 1rem;
-  height: calc(60% - 7px);
-  overflow-y: hidden;
-  background-color: #263747; // 코드 입력창 배경화면
+const StyledDiv = styled.div`
   display: flex;
-  flex-direction: column;
-  border-bottom: 0.0625rem solid #172334;
-  word-wrap: break-word;
-  word-break: break-all;
-  height: 100vh;
-`
-// const Gutter = styled.div`
-//   width: 48px;
-//   word-wrap: break-word;
-//   word-break: break-all;
-//   box-sizing: content-box;
-//   white-space: normal;
-//   height: 100%;
-//   display: inline-block;
-//   vertical-align: top;
-//   margin-bottom: -50px;
-// `
-const Changer = styled.div`
-  dispaly: flex;
-`
-
-const ResultSection = styled.div`
-  display: block;
-`
-
-// #98ABB
-
-const TopNavBar = styled.div`
-  background: ${({ theme }) => theme.codingTestBackgroundColor};
-`
-const Btn = styled.button`
-  font-size: 1.5rem;
-  color: white;
-  background-color: ${({ theme }) => theme.codingTestButtonColor};
-  round: 1;
-  border: solid 2px grey;
-  border-radius: 12px;
-  padding: 6px;
-  margin: 10px;
-`
-const ColoredLine = ({ color }) => (
-  <hr
-    style={{
-      color: color,
-      backgroundColor: color,
-      height: 5,
-    }}
-  />
-)
-
-const Foot = styled.div`
-  display: flex;
-  justify-content: space-between;
   align-items: center;
-  width: 100%;
-  margin: 0rem 0rem;
-  padding: 0rem 1rem;
+  padding: 0.2rem 0.5rem;
+  border-radius: 0.5rem;
+  font-size: 0.9rem;
+  font-weight: bold;
+  background-color: ${({ theme }) => theme.studyBgColor};
 `
 
-const EndBtn = styled.div`
+const ButtonWrapper = styled.div`
   display: flex;
-`
-
-const CompileBtn = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-left: 1rem;
-`
-const Space = styled.div`
-  width: 1.5rem;
-  height: auto;
-  display: inline-block;
+  gap: 10px;
 `
