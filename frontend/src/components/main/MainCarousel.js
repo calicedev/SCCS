a30b19347087bb3a79c2729e31b65e438dc6daf2
Minhyeok Son <mh.blosson@gmail.com>
2023-02-16 14:30:27 +0900

    [FE][feat] 배너 클릭시 about 페이지 이동

--- frontend/src/components/main/MainCarousel.js
+++ frontend/src/components/main/MainCarousel.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import styled from 'styled-components'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
+
 import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
 import { Carousel } from 'react-responsive-carousel'
 import mainBanner1 from 'assets/img/main_banner_1.png'
@@ -17,15 +20,35 @@ export default function MainCarousel() {
       infiniteLoop={true}
       interval={3000}
     >
-      <div>
-        <img src={mainBanner1} alt="배너이미지1" />
-      </div>
-      <div>
-        <img src={mainBanner2} alt="배너이미지2" />
-      </div>
-      <div>
-        <img src={mainBanner3} alt="배너이미지3" />
-      </div>
+      <NavStyle to="/about">
+        <div>
+          <img src={mainBanner1} alt="배너이미지1" />
+        </div>
+      </NavStyle>
+      <NavStyle to="/about">
+        <div>
+          <img src={mainBanner2} alt="배너이미지2" />
+        </div>
+      </NavStyle>
+      <NavStyle to="/about">
+        <div>
+          <img src={mainBanner3} alt="배너이미지3" />
+        </div>
+      </NavStyle>
     </Carousel>
   )
 }
+
+const NavStyle = styled(NavLink)`
+  margin: 1rem 0rem;
+  padding: 0rem 2rem 0rem 0rem;
+
+  font-size: 1.7rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme.secondaryFontColor};
+
+  &.active {
+    color: ${({ theme }) => theme.primaryFontColor};
+    font-weight: 600;
+  }
+`
