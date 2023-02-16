a861c3389c6484d213d36f4361aab5ae9230d808
이찬희 <dojsfffff@naver.com>
2023-02-16 10:25:24 +0900

    [BE][test] 예최 처리 테스트

--- backend/src/main/java/com/sccs/exception/UnAuthorizedException.java
+++ backend/src/main/java/com/sccs/exception/UnAuthorizedException.java
@@ -3,7 +3,7 @@ package com.sccs.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.FORBIDDEN)
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "accessToken expired")
 public class UnAuthorizedException extends RuntimeException {
     public UnAuthorizedException(String message) {
         super(message);
